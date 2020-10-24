88f590acb1fa009ae24e218d8b6f77a76937ca16
victorcarpiomrf <victorcarpio@marfeel.com>
2020-10-24 19:35:01 +0200

    Added band interaction

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,43 @@
 const { io } = require('../index');
+const Band = require('../models/Band');
+const Bands = require('../models/Bands');
+console.log('Init server');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Carpio'));
+bands.addBand(new Band('Killers'));
+bands.addBand(new Band('Amaral'));
 
 // Socket Messages
 io.on('connection', client => {
   console.log('Client connected');
 
+  client.emit('active-bands', bands.getBands());
+
+  client.on('vote-band', payload => {
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('add-band', payload => {
+    bands.addBand(new Band(payload.name));
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('delete-band', payload => {
+    bands.deleteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  })
+
   client.on('disconnect', () => {
     console.log('Client disconnected');
   });
 
-  client.on('message', data => {
-    console.log('Message here:', data);
+  client.on('new-message', payload => {
+    console.log('Message here:', payload);
 
-    io.emit('message', { admin: 'New message' });
+    client.broadcast.emit('new-message', { data: 'New message' });
   });
 });
